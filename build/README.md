ab33ef659f63a9c5d530d4091777836be79b60f5
Derek Louie <derekl@yahoo-inc.com>
2014-06-05 18:57:57 -0700

    Gulp File improvements:
    Now working when you add or delete something in the javascript folder

---
+++ build/README.md
@@ -0,0 +1,2 @@
+# To get gulp working:
+1) `npm install -g gulp`
--- build/gulpfile.js
+++ build/gulpfile.js
@@ -1,34 +1,48 @@
-// include gulp
+// Project Requirements
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var jshint = require('gulp-jshint');
 var sass = require('gulp-sass');
-var JSPath = '../assets/javascript/**/*.js';
-var CSSPath = '../assets/scss/**/*.scss';
 var spawn = require('child_process').spawn;
 var stylish = require('jshint-stylish');
+var plumber = require('gulp-plumber');
+var _ = require('underscore');
 var node;
 
-gulp.task('bundle', function() {
+
+// Config stuff:
+// Specific order that creation of files in javascript folder and in subfolders trigger the watch
+// Refer to: http://stackoverflow.com/questions/21386940/why-does-gulp-src-not-like-being-passed-an-array-of-complete-paths-to-files
+var JSBase = '../assets/javascript/';
+var JSRelPaths = ['testing/*.js','*.js','**/*.js'];
+// var JSRelPaths = ['**/*.js','testing/*.js','*.js'];
+var JSPaths = _.map(JSRelPaths, function(path) {return JSBase+path;});
+var CSSBase = '../assets/scss/';
+var CSSRelPaths = ['*.scss','**/*.scss'];
+var CSSPaths = _.map(CSSRelPaths, function(path) {return CSSBase+path;});
+
+gulp.task('bundle_js', function() {
   if (!node) {
     gulp.start('fb-flo');
   }
-  gulp.src(['../assets/javascript/testing/*.js', JSPath])
+  gulp.src(JSPaths, {base: JSBase })
+    .pipe(plumber({errorHandler: onError}))
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'))
     .pipe(jshint.reporter('fail'))
-    .on('error', onError)
     .pipe(concat('bundle.js'))
     .pipe(gulp.dest('../site'));
 });
 
 // Compile Our Sass
-gulp.task('css', function() {
-    gulp.src([CSSPath])
+gulp.task('bundle_css', function() {
+    gulp.src(CSSPaths, {base: CSSBase})
     .pipe(sass())
     .pipe(concat('bundle.css'))
     .pipe(gulp.dest('../site'));
 });
+
+gulp.task('bundle', ['bundle_js','bundle_css']);
 
 gulp.task('fb-flo', function() {
     if (node) {
@@ -36,18 +50,22 @@ gulp.task('fb-flo', function() {
     }
     node = spawn('node', ['flo.js'], {stdio: 'inherit'});
     node.on('close', function (code) {
-    if (code === 8) {
-      gulp.log('Error detected, turning off fb-flo...');
-    }
+        if (code === 8) {
+          gulp.log('Error detected, turning off fb-flo...');
+        }
   });
 });
 
-gulp.task('watch', function() {
-    gulp.watch(JSPath, ['bundle']);
-    gulp.watch(CSSPath, ['css']);
+gulp.task('watch_js', function() {
+    gulp.watch(JSPaths, ['bundle_js']); 
+});
+
+gulp.task('watch_css', function() {
+    gulp.watch(CSSPaths, ['bundle_css']); 
 });
 
 function onError (err) {
+    console.log(err);
     // kill $(ps aux | grep 'node flo' | awk '{print $2}')
     if (node) {
         node.kill();
@@ -55,4 +73,5 @@ function onError (err) {
     }
 }
 
-gulp.task('default', ['watch','fb-flo']);
+// gulp.task('default', ['bundle','watch_js','watch_css','fb-flo']);
+gulp.task('default', ['bundle','watch_js','fb-flo']);
